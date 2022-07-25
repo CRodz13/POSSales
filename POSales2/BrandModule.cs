b7b63e707418b0b5af86e272a6a109879f4f1465
crodz13 <rodcee13@gmail.com>
2022-07-24 22:28:45 -0400

    Uploading code

---
+++ POSales2/BrandModule.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSales2
+{
+    public partial class BrandModule : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        DBConnect dbcon = new DBConnect();
+        Brand brand;
+
+        public BrandModule(Brand br)
+        {
+            InitializeComponent();
+            cn = new SqlConnection(dbcon.myConnection());
+            brand = br;
+        }
+
+        private void BrandModule_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // To insert brand name to brand table
+            try
+            {
+                if (MessageBox.Show("Are you sure you want to save this brand?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    cn.Open();
+                    cm = new SqlCommand("INSERT INTO tblBrand(brand)VALUES(@brand)", cn);
+                    cm.Parameters.AddWithValue("@brand", txtBrand.Text);
+                    cm.ExecuteNonQuery();
+                    cn.Close();
+                    MessageBox.Show("Record has been successful saved.", "POS");
+                    Clear();
+                    brand.LoadBrand();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            txtBrand.Clear();
+            btnUpdate.Enabled = false;
+            btnSave.Enabled = true;
+            txtBrand.Focus();
+        }
+
+        private void txtBrand_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //Update Brand name
+            if (MessageBox.Show("Are you sure you want to update this brand?", "Update Record!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                cn.Open();
+                cm = new SqlCommand("UPDATE tblBrand SET brand = @brand WHERE id LIKE'" + lblId.Text + "'", cn);
+                cm.Parameters.AddWithValue("@brand", txtBrand.Text);
+                cm.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("Brand has been successfully updated.", "POS");
+                Clear();
+                this.Dispose();// To close this form after update date
+            }
+
+        }
+
+        private void lblId_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
